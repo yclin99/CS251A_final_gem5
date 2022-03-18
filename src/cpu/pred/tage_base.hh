d55c897233e02f6700a889b56fed20bdf7953515
YuanChingLin <ken1277725@gmail.com>
2022-03-18 09:33:21 -0700

    fix

--- src/cpu/pred/tage_base.hh
+++ src/cpu/pred/tage_base.hh
@@ -117,14 +117,13 @@ class TAGEBase : public SimObject
             compressorTablesEntriesSize = 1 << compressorTablesEntriesLength;
             compressorInputLength = compressorTablesEntriesLength;
             compressorTables = new int[compressorTablesEntriesSize];
+            compressorTableInit();
 
             //compress history
             compressedHistoryLength = compressed_length / compressorTablesEntriesLength * compressorOutputLength;
             compressedHistory = new uint8_t[compressedHistoryLength + 1];
             compressOutPoint = compressedHistoryLength % compLength2;
-            for(int i = 0; i < compressorTablesEntriesSize; ++i){
-                compressorTables[i] = i % (1 << compressorOutputLength);
-            }
+
             // left 4 bits for compressor
             origLength = original_length - compressorInputLength;
             compLength  = compressed_length;
@@ -150,7 +149,6 @@ class TAGEBase : public SimObject
             comp1 ^= h[origLength] << outpoint;
             comp1 ^= (comp1 >> compLength1);
             comp1 &= (1ULL << compLength1) - 1;
-
             comp = comp1 ^ comp2;
 
         }
@@ -166,6 +164,11 @@ class TAGEBase : public SimObject
                 comp2 = (comp2 << 1) | compressedHistory[i];
                 comp2 ^= (comp2 >> compLength2);
                 comp2 &= (1ULL << compLength2) - 1;
+            }
+        }
+        void compressorTableInit(){
+            for(int i = 0; i < compressorTablesEntriesSize; ++i){
+                compressorTables[i] = i % (1 << compressorOutputLength);
             }
         }
     };
